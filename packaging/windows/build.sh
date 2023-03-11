cae4b73f19e2e56006690452beb01e4e263e33e6
MatMoul <matmoul@gmail.com>
2023-03-11 03:09:04 +0100

    Add packaging folder

--- packaging/windows/build.sh
+++ packaging/windows/build.sh
@@ -2,15 +2,20 @@
 
 declare -r APPNAME=dokytree
 declare -r NODEVER=19.7.0
-declare TMPDIR=""
+declare BASEDIR=""
 declare CURDIR=""
+declare TMPDIR=""
 
-CURDIR=$(pwd)
+BASEDIR="$(pwd)"/$(dirname "${0}")/../..
+echo "${BASEDIR}"
+
+CURDIR+="$(pwd)"
+
 TMPDIR=$(mktemp -d -u)
 mkdir -p "${TMPDIR}"/"${APPNAME}"
 
-cp -R src/* "${TMPDIR}"/"${APPNAME}"
-cp -R samples "${TMPDIR}"/"${APPNAME}"
+cp -R "${BASEDIR}"/src/* "${TMPDIR}"/"${APPNAME}"
+cp -R "${BASEDIR}"/samples "${TMPDIR}"/"${APPNAME}"
 
 cd "${TMPDIR}" || exit
 wget https://nodejs.org/dist/v${NODEVER}/node-v${NODEVER}-win-x64.zip
@@ -18,6 +23,6 @@ unzip node-v${NODEVER}-win-x64.zip
 cp node-v${NODEVER}-win-x64/node.exe "${TMPDIR}"/"${APPNAME}"
 
 zip -r "${CURDIR}"/"${APPNAME}".zip "${APPNAME}"
-cd "${CURDIR}" || cd || exit
 
+cd "${CURDIR}" || cd || exit
 rm -rf "${TMPDIR}"
